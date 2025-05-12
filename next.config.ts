b829b44cd9e451022cd6eda666a9768a9a3ecac5
sirbenchalot757 <kyleforkey@ethmint.io>
2025-05-12 12:02:36 -0500

    reset

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
-import { createContentlayerPlugin } from "next-contentlayer2"
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-const withContentlayer = createContentlayerPlugin({
-  // Additional Contentlayer config options
-})
-
-export default withContentlayer(nextConfig)
+export default nextConfig
